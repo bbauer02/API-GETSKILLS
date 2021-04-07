60fbe16c87b182dad16b92c7b3dd38d354883bf8
bbauer02 <bbauer02@gmail.com>
2021-04-08 00:20:56 +0200

    findAllLevels : Ajout du filtre sur le label, la référence. Ajout limit et offset.

--- src/routes/level/findAllLevels.js
+++ src/routes/level/findAllLevels.js
@@ -1,17 +1,53 @@
 ﻿const {models} = require('../../models');
+const { Op,Utils } = require('sequelize');
 
 module.exports =  (app) => {
-    app.get('/api/levels', async (req,res) => {
-       try {
-            const Levels = await models['Level'].findAndCountAll({order:['label']});
-            const message = `${Levels.count} level(s) found`;
-            res.json({message, data: Levels.rows})
-       }
-       catch (error){
-         const message = `Service not available. Please retry later.`;
-         res.status(500).json({message, data: error})
-       }
+  app.get('/api/levels', async (req,res) => {
+    try {
+      // calcul le nombre d'arguments dans la requête
+      const countArgs = Object.keys(req.query).length;
+      const parameters = {};
+      // Parameter : LABEL
+      if(req.query.label) {
+        parameters.where = {
+            label:{
+            [Op.like] : `%${req.query.label}%`
+        }}
+      }
+      // Parameter : REF
+      if(req.query.ref) {
+        parameters.where = {
+            ref:{
+            [Op.like] : `%${req.query.ref}%`
+        }}
+      }
+    
+      // Parameter : LIMIT
+      if(req.query.limit) {
+        const limit = parseInt(req.query.limit);       
+        if(isNaN(limit) ) {
+          const message = `Limit parameter should be an integer.`;
+          return res.status(400).json({message})
+        }
+        parameters.limit = parseInt(req.query.limit);
+      }
+      // Parameter : OFFSET
+      if(req.query.offset) {
+        const offset = parseInt(req.query.offset);       
+        if(isNaN(offset) ) {
+          const message = `Offset parameter should be an integer.`;
+          return res.status(400).json({message})
+        }
+        parameters.offset = parseInt(req.query.offset);
+      }
 
-
-    });
-}
+      const Levels = await models['Level'].findAndCountAll(parameters);
+      const message = `${Levels.count} level(s) found`;
+      res.json({message, data: Levels.rows})
+    }
+    catch (error){
+      const message = `Service not available. Please retry later.`;
+      res.status(500).json({message, data: error})
+    }
+  });
+  }
