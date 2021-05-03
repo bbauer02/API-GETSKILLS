f766aa2796f81a57bfd2a2891e9d254293f17e7e
Bauer <bbauer02@gmail.com>
2021-05-03 09:57:06 +0200

    Mise en place d'un fichier de variables d'environnement pour la connection à la BDD

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,12 +1,15 @@
 ﻿const {Sequelize, DataTypes} = require('sequelize');
+
+const ENV = require('dotenv').config().parsed;
+
 const sequelize = new Sequelize(
-    'gettested',
-    'root',
-    '',
+    ENV.DB_NAME,
+    ENV.DB_USER,
+    ENV.DB_PWD,
     {
-        host: 'localhost',
-        port: 3307,
-        dialect: 'mariadb',
+        host: ENV.DB_HOST,
+        port: ENV.DB_PORT,
+        dialect: ENV.DB_DIALECT,
         define: {
             charset: 'utf8mb4',
             collate: 'utf8mb4_general_ci',
