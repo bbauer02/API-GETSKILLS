260ef71d64a7e0f559524774c27db7ee3e265f23
Bauer <bbauer02@gmail.com>
2021-05-17 13:28:36 +0200

    Mise en place de CORS- Correctif Bug 4

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcrypt');
 const jwtUtils = require('../auth/jwt.utils');
 module.exports = (app) => {
     app.post('/api/login',async (req, res) => {
-        console.log(req.body)
         try {
             const user = await models['User'].findOne({
                 where:
