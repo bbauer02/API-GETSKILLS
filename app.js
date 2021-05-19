c1424acef69e83eee952d662f2f0279c16fc1629
Bauer <bbauer02@gmail.com>
2021-05-19 15:02:24 +0200

    Recupération du travail distant

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const models = require('./src/models');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
- 
-
+var cookieParser = require('cookie-parser');
+var jwt = require('express-jwt');
+const ENV = require('dotenv').config().parsed;
+let JWT_SIGN_SECRET = ENV.JWT_SIGN_SECRET;
 
 app
     .use(express.urlencoded({ extended: true }))
     .use(express.json())
     .use(cors())
+    .use(cookieParser())
+    .use(jwt({ secret: JWT_SIGN_SECRET, algorithms: ['HS256'] , getToken: req => req.cookies.token}))
 
 // Initialisation de la BDD
 models.initDB(sequelize);
