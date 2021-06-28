28aaf78e07621069b08d663d23fe6c635ba15494
paulpiazza <paul.piazza.pro@gmail.com>
2021-06-28 08:45:23 +0200

    ajout isArchive sur TEST

--- src/db/mock-tests.js
+++ src/db/mock-tests.js
@@ -3,33 +3,43 @@
         id:1,
         label: "DELF",
         isInternal: false,
-        parent_id:null
-        
+        parent_id:null,
+        isArchive: false,
     },
     {
         id:2,
         label: "DELF-NATURALISATION",
         isInternal: false,
-        parent_id: 1       
+        parent_id: 1,
+        isArchive: false,
     },
     {
         id:3,
         label: "JLPT",
         isInternal: false,
-        parent_id:null
+        parent_id:null,
+        isArchive: false,
     },
     {
         id:4,
         label: "TOIEC",
         isInternal: false,
-        parent_id:null
-        
+        parent_id:null,
+        isArchive: false,
     },
     {
         id:5,
         label: "DELF-ENFANT",
         isInternal: false,
-        parent_id: 1       
+        parent_id: 1,
+        isArchive: false,
+    },
+    {
+        id:6,
+        label: "TEST (ARCHIVE)",
+        isInternal: false,
+        parent_id: null,
+        isArchive: true,
     },
 ]
 
