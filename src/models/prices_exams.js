d1c23093599741f32926665870dc225222c932b3
Paul Piazza <paul.piazza.pro@gmail.com>
2021-07-13 09:08:34 +0200

    implémentation de la table prices_exams

---
+++ src/models/prices_exams.js
@@ -0,0 +1,20 @@
+﻿const {models} = require("./index");
+module.exports = (sequelize, DataTypes) => {
+    const ExamsPrice = sequelize.define('ExamsPrice', {
+        institut_id: {
+            type: DataTypes.INTEGER,
+        },
+        exam_id: {
+            type: DataTypes.INTEGER,
+        },
+
+        price: {
+            type: DataTypes.FLOAT,
+        },
+    }, {
+        tableName: 'prices_exams',
+        timestamps: false,
+    });
+
+    return ExamsPrice;
+}   
