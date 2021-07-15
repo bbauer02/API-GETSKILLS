738ea79660dbdcfbf807fdc4d53e19091fa6eba4
Paul Piazza <paul.piazza.pro@gmail.com>
2021-07-15 14:33:04 +0200

    modif du get

--- src/routes/pricesExams/getAllPricesExamsByInstitut.js
+++ src/routes/pricesExams/getAllPricesExamsByInstitut.js
@@ -39,21 +39,34 @@ module.exports = (app) => {
 
 
         // récupérer tous les exams pour un institut
-        // `SELECT i.label as 'institut', t.label as 'test', e.label as 'exam', p.price FROM instituts as i JOIN prices_exams as p ON i.institut_id = p.institut_id JOIN exams as e ON p.exam_id = e.exam_id JOIN tests as t ON e.test_id = t.test_id WHERE i.institut_id = ${institutId}`
+        // sequelize.query(`SELECT i.label as 'institut', t.label as 'test', e.label as 'exam', p.price FROM instituts as i INNER JOIN prices_exams as p ON i.institut_id = p.institut_id INNER JOIN exams as e ON p.exam_id = e.exam_id INNER JOIN tests as t ON e.test_id = t.test_id WHERE i.institut_id = ${institutId}`)
+
         await models['Test'].findAndCountAll({
-            attributes: ['label'],
+            attributes: ['test_id', 'label'],
             required: true,
             include: [{
                 model: models['Exam'],
-                attributes: ['label'],
+                attributes: ['exam_id', 'label'],
                 required: true,
                 include: [{
-                    attributes: ['label'],
+                    attributes: ['institut_id', 'label'],
                     model: models['Institut'],
                     where: {institut_id: institutId},
                     required: true,
                 }]
             }]
+        }).then(function (pricesFound) {
+            return pricesFound.rows.reduce(function (prev, curr) {
+                curr.dataValues.Exams.forEach((exam) => {
+                    let item = {test_id: '', institut_id: '', exam_id: '', price: ''};
+                    item.test_id = curr.dataValues.test_id;
+                    item.institut_id = exam.Instituts[0].ExamsPrice.institut_id;
+                    item.exam_id = exam.Instituts[0].ExamsPrice.exam_id;
+                    item.price = exam.Instituts[0].ExamsPrice.price
+                    prev = [...prev, item];
+                })
+                return prev;
+            }, [])
         }).then(function (pricesFound) {
             const message = `${pricesFound.count} price(s) found`;
             return res.json({message, data: pricesFound})
