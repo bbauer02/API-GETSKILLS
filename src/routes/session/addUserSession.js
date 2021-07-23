95a71369461a732178b54b6398ca36b52c88e9c1
debe0025 <jules.debeaumont@etudiant.univ-reims.fr>
2021-07-23 12:27:22 +0200

    fix addUserSession

--- src/routes/session/addUserSession.js
+++ src/routes/session/addUserSession.js
@@ -5,9 +5,44 @@ const { isAuthenticated, isAuthorized } = require('../../auth/jwt.utils');
 module.exports = (app) => {
     app.post('/api/sessions/users',  isAuthenticated,isAuthorized, async (req,res) => {
         try{
-            const sessionUser = await models['sessionUser'].create(req.body);
-            const message = `User id: ${sessionUser.user_id} has been add in the session id : ${sessionUser.session_id}.`;
-            res.json({message, data:sessionUser})
+            // req.body = object avec values et session
+            const sessionUser = await models['sessionUser'].create(req.body.values);
+
+            // initialiser les parameters pour trouver les exams
+            // 1
+            const parameters = {};
+            parameters.where = {};
+
+
+            // trouver les exams depuis test id et level id(si pas null) depuis le req.body
+            // 2
+            const test = parseInt(req.body.session.test_id);
+            parameters.where.test_id = test;
+
+            if (req.body.session.level_id !== "null") {
+                const level = parseInt(req.body.session.level_id); 
+                parameters.where.level_id = level;
+            }
+
+            const Exams = await models['Exam'].findAndCountAll(parameters);
+
+
+            // post les sessionsUserOptions en bulk
+            // 3
+            let sessionUsersOptionsForCreate = [];
+            // console.log(Exams.rows);
+            Exams.rows.forEach((exam, index) => {
+                console.log("examId =", exam.dataValues.exam_id, "index =", index);
+                sessionUsersOptionsForCreate[index] = {};
+                sessionUsersOptionsForCreate[index].exam_id = exam.dataValues.exam_id;
+                sessionUsersOptionsForCreate[index].isCandidate = exam.dataValues.isOption === true ? false: true;
+                sessionUsersOptionsForCreate[index].sessionUser_id = sessionUser.sessionUser_id;
+            })
+            
+            const sessionUserOptions = await models['sessionUserOption'].bulkCreate(sessionUsersOptionsForCreate);
+
+            const message = `User id: ${sessionUser.user_id} has been added in the session id : ${sessionUser.session_id}.`;
+            res.json({message, data: sessionUser});
         }catch(error) {
             if(error instanceof ValidationError) {
                 return res.status(400).json({message:error.message, data:error})
