e0b8971e6df69c8afbebb204baefd10b5b51b5ea
debe0025 <jules.debeaumont@etudiant.univ-reims.fr>
2021-07-29 15:16:15 +0200

    delete session CASCADE for sessionUser and sessionUserOption + archives for all of theses

--- src/models/session.js
+++ src/models/session.js
@@ -1,83 +1,91 @@
-﻿module.exports = (sequelize, DataTypes) => {
-    const Session =  sequelize.define('Session', {
+﻿const {models} = require("./index");
+module.exports = (sequelize, DataTypes) => {
+    const Session = sequelize.define('Session', {
         session_id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true,
-            allowNull:false
+            allowNull: false
         },
         institut_id: {
             type: DataTypes.INTEGER,
-            allowNull:false,
-            validate : {
-                notEmpty:{msg: `Session:institut_id cannot be empty!`},
-                notNull: {msg: `Session:institut_id cannot be NULL!`}
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: `Session:institut_id cannot be empty!` },
+                notNull: { msg: `Session:institut_id cannot be NULL!` }
             }
         },
         start: {
             type: DataTypes.DATE,
-            allowNull:false,
-            validate : {
-                notEmpty:{msg: `Session:start cannot be empty!`},
-                notNull: {msg: `Session:start cannot be NULL!`}
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: `Session:start cannot be empty!` },
+                notNull: { msg: `Session:start cannot be NULL!` }
             }
         },
         end: {
             type: DataTypes.DATE,
-            allowNull:false,
-            validate : {
-                notEmpty:{msg: `Session:end cannot be empty!`},
-                notNull: {msg: `Session:end cannot be NULL!`}
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: `Session:end cannot be empty!` },
+                notNull: { msg: `Session:end cannot be NULL!` }
             }
         },
         limitDateSubscribe: {
             type: DataTypes.DATE,
-            allowNull:false,
-            validate : {
-                notEmpty:{msg: `Session:limitDateSubscribe cannot be empty!`},
-                notNull: {msg: `Session:limitDateSubscribe cannot be NULL!`}
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: `Session:limitDateSubscribe cannot be empty!` },
+                notNull: { msg: `Session:limitDateSubscribe cannot be NULL!` }
             }
         },
         placeAvailable: {
             type: DataTypes.INTEGER,
-            allowNull:false,
-            validate : {
-                notEmpty:{msg: `Session:placeAvailable cannot be empty!`},
-                notNull: {msg: `Session:placeAvailable cannot be NULL!`}
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: `Session:placeAvailable cannot be empty!` },
+                notNull: { msg: `Session:placeAvailable cannot be NULL!` }
             }
         },
         validation: {
             type: DataTypes.BOOLEAN,
-            allowNull:false,
+            allowNull: false,
             defaultValue: false,
-            validate : {
-                notEmpty:{msg: `Session:validation cannot be empty!`},
-                notNull: {msg: `Session:validation cannot be NULL!`}
+            validate: {
+                notEmpty: { msg: `Session:validation cannot be empty!` },
+                notNull: { msg: `Session:validation cannot be NULL!` }
             }
         },
-        test_id:{
+        test_id: {
             type: DataTypes.INTEGER,
-            allowNull:false,
-            validate : {
-                notEmpty:{msg: `Session:testLevel_id cannot be empty!`},
-                notNull: {msg: `Session:testLevel_id cannot be NULL!`}
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: `Session:testLevel_id cannot be empty!` },
+                notNull: { msg: `Session:testLevel_id cannot be NULL!` }
             }
         },
-        level_id:{
+        level_id: {
             type: DataTypes.INTEGER,
-            allowNull:true
-        } 
+            allowNull: true
+        }
     },
-    {
-        tableName: 'sessions',
-        timestamps: false
-    });
-    Session.associate = models => { 
-        Session.hasMany(models.sessionUser,{foreignKey:'session_id', sourceKey:'session_id'})
-        Session.belongsTo(models.Institut,{foreignKey:'institut_id',sourceKey: 'institut_id'});
-        Session.belongsTo(models.Test,{foreignKey:'test_id'});
-        Session.belongsTo(models.Level,{foreignKey:'level_id'});
+        {
+            tableName: 'sessions',
+            timestamps: false,
+            hooks: {
+                beforeDestroy (instance, options) {
+                    // console.log(instance.dataValues);
+                    models['SessionHist'].create(instance.dataValues);
+                }
+            }
+        });
+
+    Session.associate = models => {
+        Session.hasMany(models.sessionUser, { foreignKey: 'session_id', sourceKey: 'session_id', onDelete:'CASCADE'})
+        Session.belongsTo(models.Institut, { foreignKey: 'institut_id', sourceKey: 'institut_id' });
+        Session.belongsTo(models.Test, { foreignKey: 'test_id' });
+        Session.belongsTo(models.Level, { foreignKey: 'level_id' });
     };
+
     return Session;
 }
-   
