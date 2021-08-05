745d7db7ab32b242e8ef6ac060f5ffc2e0e4577b
debe0025 <deb.jules@outlook.fr>
2021-08-05 15:40:00 +0200

    fixed findEmpowermentTestByInstituts and CreateEmpowerment

--- src/routes/empowerementTests/findEmpowermentTestsByInstituts.js
+++ src/routes/empowerementTests/findEmpowermentTestsByInstituts.js
@@ -1,56 +1,82 @@
 ﻿const { models } = require('../../models');
 const { isAuthenticated, isAuthorized } = require('../../auth/jwt.utils');
 
+// TODO: isAuthenticated, isAuthorized
 module.exports = (app) => {
     app.get('/api/instituts/:idInstitut/empowermentTests', async (req, res) => {
-        try {
-            const parameters = {};
-            parameters.where = {};
 
-            const institutId = parseInt(req.params.idInstitut);
-            if (isNaN(institutId)) {
-                const message = `Institut parameter should be an integer.`;
-                return res.status(400).json({ message })
-            }
-            parameters.where.institut_id = institutId;
+        // PARAMETERS
+        const parameters = {};
+        parameters.where = {};
 
-            // vérifier l'institut
-            await models['Institut'].findOne({
-                where: { institut_id: institutId }
-            }).then(function (institutFound) {
+        const institutId = parseInt(req.params.idInstitut);
+        if (isNaN(institutId)) {
+            const message = `Institut parameter should be an integer.`;
+            return res.status(400).json({ message })
+        }
+        parameters.where.institut_id = institutId;
+
+        parameters.include = [{
+            model: models['User'],
+            attributes: { exclude: ['password'] },
+            include: [{
+                model: models['institutHasUser'],
+                attributes: ['institut_id'],
+                where: [{ institut_id: institutId }],
+                as: 'instituts',
+                include: [{
+                    model: models['Role'],
+                    attributes: ['role_id', 'label', 'power']
+                }]
+            }]
+        },
+        {
+            model: models['Test'],
+            attributes: ["label"]
+        }];
+
+
+        async function checkInstitut() {
+            try {
+                // vérifier l'institut
+                const institutFound = await models['Institut'].findOne({
+                    where: { institut_id: institutId }
+                });
+
                 if (institutFound === null) {
                     const message = `institut doesn't exist. Retry with an other institut id.`;
                     return res.status(404).json({ message });
+                } else {
+                    return institutFound;
                 }
-            }).catch(function (error) {
-                const message = `Service not available. Please retry later.`;
+
+            } catch (error) {
+                const message = `An error has occured while cheking the institut_id`;
                 return res.status(500).json({ message, data: error.message })
-            });
-
-            parameters.include = [{
-                model: models['User'],
-                attributes: {exclude:['password']},
-                include: [{
-                    model: models['institutHasUser'],
-                    attributes:['institut_id'],
-                    where: [{institut_id : institutId}],
-                    as:'instituts',
-                    include:[{
-                        model:models['Role'],
-                        attributes:['role_id','label','power']
-                    }]
-                }]},
-            {
-                model: models['Test'],
-                attributes: ["label"]
-            }];
-
-
-            const EmpowermentTests = await models['empowermentTests'].findAll(parameters);
-            const message = `${EmpowermentTests.length} EmpowermentTests from institut ${institutId} found`;
-            res.json({ message, data: EmpowermentTests })
+            }
         }
-        catch (error) {
+
+
+        async function findEmpowermentsByInstitut(_parameters) {
+            try {
+                const EmpowermentTestsFound = await models['empowermentTests'].findAll(_parameters);
+                return EmpowermentTestsFound;
+
+            } catch (error) {
+                const message = `An error has occured while looking for empowerment of this institut`;
+                return res.status(500).json({ message, data: error.message })
+            }
+        }
+
+        try {
+
+            await checkInstitut();
+            const EmpowermentTestsFound = await findEmpowermentsByInstitut(parameters);
+
+            const message = `${EmpowermentTestsFound.length} EmpowermentTests from institut ${institutId} found`;
+            res.json({ message, data: EmpowermentTestsFound })
+
+        } catch (error) {
             const message = `Service not available. Please retry later.`;
             res.status(500).json({ message, data: error })
         }
