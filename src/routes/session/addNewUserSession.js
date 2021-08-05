414fb33b17b282c6efd713ea0974a292ed0f8f63
debe0025 <deb.jules@outlook.fr>
2021-08-05 15:01:06 +0200

    addNewUserInstitut done and fix addNewUserSession

--- src/routes/session/addNewUserSession.js
+++ src/routes/session/addNewUserSession.js
@@ -1,14 +1,14 @@
 const { ValidationError, UniqueConstraintError } = require('sequelize');
 const { models } = require('../../models');
-const asyncLib = require('async');
 const { isAuthenticated, isAuthorized } = require('../../auth/jwt.utils');
 // TO DO add isAuthenticated & isAuthorized
 
 module.exports = (app) => {
     app.post('/api/instituts/:idInstitut/sessions/:idSession/newUser', async (req, res) => {
 
-        asyncLib.waterfall([
-            function (done) {
+
+        async function createUser() {
+            try {
 
                 // 0 - Créer User
                 const valuesForPostUser = { ...req.body };
@@ -18,67 +18,70 @@ module.exports = (app) => {
                 delete valuesForPostUser.paymentMode;
                 delete valuesForPostUser.passwordConfirmation;
 
-                models['User'].create(valuesForPostUser)
-                    .then(function (userCreated) {
-                        done(null, userCreated)
-                    })
-                    .catch(function (error) {
-                        const message = `An error has occured creating the User.`;
-                        return res.status(500).json({ message, data: error.message })
-                    });
-            },
+                const userCreated = await models['User'].create(valuesForPostUser);
+                return userCreated;
+
+            } catch (error) {
+                const message = `An error has occured creating the User.`;
+                return res.status(500).json({ message, data: error.message })
+            }
+        }
 
 
-
-            function (userCreated, done) {
+        async function postInstitutHasUser(_userCreated) {
+            try {
 
                 // 1 - Post Institut has user
                 const valuesForPostInstitutHasUser = {};
                 valuesForPostInstitutHasUser.role_id = 1;
-                valuesForPostInstitutHasUser.user_id = userCreated.dataValues.user_id;
-                valuesForPostInstitutHasUser.institut_id = req.params.idInstitut;
+                valuesForPostInstitutHasUser.user_id = _userCreated.dataValues.user_id;
+                valuesForPostInstitutHasUser.institut_id = Number(req.params.idInstitut);
 
-                models['institutHasUser'].create(valuesForPostInstitutHasUser)
-                    .then(function (institutHasUserCreated) {
-                        done(null, userCreated, institutHasUserCreated)
-                    })
-                    .catch(function (error) {
-                        const message = `An error has occured creating the institutUser.`;
-                        return res.status(500).json({ message, data: error.message })
-                    });
-            },
+                const institutHasUserCreated = await models['institutHasUser'].create(valuesForPostInstitutHasUser);
+                return institutHasUserCreated;
 
+            } catch (error) {
+                const message = `An error has occured creating the institutUser.`;
+                return res.status(500).json({ message, data: error.message })
+            }
+        }
 
 
-            function (userCreated, institutHasUserCreated, done) {
+        async function checkSessionPlaceAvailable() {
+            try {
 
                 // 2 - Check si assez de place dans la session
-                models['Session'].findOne({
+                const session = await models['Session'].findOne({
                     where: { session_id: req.params.idSession },
                     include: {
                         model: models['sessionUser'],
                         as: "sessionUsers",
                         attributes: ['user_id']
                     }
-                }).then(function (session) {
-                    if (session === null) {
-                        const message = `session doesn't exist. Retry with an other session id.`;
-                        return res.status(404).json({ message });
-                    } else if (session.dataValues.sessionUsers.length + 1 > session.dataValues.placeAvailable) {
-                        const message = `session is full!`;
-                        return res.status(500).json({ message });
-                    } else {
-                        done(null, userCreated, institutHasUserCreated, session)
-                    }
-                }).catch(function (error) {
-                    const message = `An error has occured finding the session.`;
-                    return res.status(500).json({ message, data: error.message })
                 });
-            },
+
+                if (session === null) {
+                    const message = `session doesn't exist. Retry with an other session id.`;
+                    return res.status(404).json({ message });
+                } else if (session.dataValues.sessionUsers.length + 1 > session.dataValues.placeAvailable) {
+                    const message = `session is full!`;
+                    return res.status(500).json({ message });
+                } else {
+                    return true;
+                }
+
+            } catch (error) {
+                const message = `An error has occured finding the session.`;
+                return res.status(500).json({ message, data: error.message })
+            }
+        }
 
 
 
-            function (userCreated, institutHasUserCreated, session, done) {
+
+        async function findAllSessionExams() {
+            try {
+
                 // 3 - initialiser les parameters pour trouver les exams
                 const parameters = {};
                 parameters.where = {};
@@ -92,63 +95,75 @@ module.exports = (app) => {
                     const level = parseInt(req.body.level_id);
                     parameters.where.level_id = level;
                 }
-                models['Exam'].findAndCountAll(parameters)
-                    .then(function (examsFound) {
-                        done(null, userCreated, institutHasUserCreated, session, examsFound)
-                    })
-                    .catch(function (error) {
-                        const message = `An error has occured finding the exams.`;
-                        return res.status(500).json({ message, data: error.message })
-                    })
-            },
+                const allExamsFromSession = await models['Exam'].findAndCountAll(parameters);
+                return allExamsFromSession;
+
+            } catch (error) {
+                const message = `An error has occured finding the exams.`;
+                return res.status(500).json({ message, data: error.message })
+            }
+        }
 
 
-
-
-            function (userCreated, institutHasUserCreated, session, examsFound, done) {
+        async function postSessionUser(_userCreated) {
+            try {
 
                 // 5 - Créer le sessionUser req.body = object avec values et session
                 const valuesForPostSessionUser = {};
-                valuesForPostSessionUser.user_id = userCreated.dataValues.user_id;
+                valuesForPostSessionUser.user_id = _userCreated.dataValues.user_id;
                 valuesForPostSessionUser.session_id = req.params.idSession;
                 valuesForPostSessionUser.hasPaid = req.body.hasPaid;
                 valuesForPostSessionUser.paymentMode = req.body.paymentMode;
 
-                models['sessionUser'].create(valuesForPostSessionUser)
-                    .then(function (sessionUserCreated) {
-                        done(null, userCreated, institutHasUserCreated, session, examsFound, sessionUserCreated)
-                    }).catch(function (error) {
-                        const message = `An error has occured creating the sessionUser.`;
-                        return res.status(500).json({ message, data: error.message })
-                    });
-            },
+                const sessionUserCreated = await models['sessionUser'].create(valuesForPostSessionUser);
+                return sessionUserCreated
+
+            } catch (error) {
+                const message = `An error has occured creating the sessionUser.`;
+                return res.status(500).json({ message, data: error.message })
+            }
+        }
 
 
 
-
-            function ( userCreated, institutHasUserCreated, session, examsFound, sessionUserCreated, done) {
+        async function postAllUserOptions(_sessionUserCreated, _allExamsFromSession) {
+            try {
 
                 // 6 - créer l'object pour le bulk
                 let sessionUsersOptionsForCreate = [];
-                examsFound.rows.forEach((exam, index) => {
+                _allExamsFromSession.rows.forEach((exam, index) => {
                     sessionUsersOptionsForCreate[index] = {};
                     sessionUsersOptionsForCreate[index].exam_id = exam.dataValues.exam_id;
                     sessionUsersOptionsForCreate[index].isCandidate = exam.dataValues.isOption === true ? false : true;
-                    sessionUsersOptionsForCreate[index].sessionUser_id = sessionUserCreated.dataValues.sessionUser_id;
+                    sessionUsersOptionsForCreate[index].sessionUser_id = _sessionUserCreated.dataValues.sessionUser_id;
                 })
 
 
                 // 7 - post les sessionsUserOptions en bulk
-                models['sessionUserOption'].bulkCreate(sessionUsersOptionsForCreate)
-                    .then(function (optionsCreated) {
-                        done(null, userCreated, institutHasUserCreated, session, examsFound, sessionUserCreated, optionsCreated);
-                        const message = `User id: ${userCreated.dataValues.user_id} has been added in the session id : ${session.dataValues.session_id}.`;
-                        res.json({ message, data: optionsCreated });
-                    }).catch(function (error) {
-                        const message = `An error has occured creating the options.`;
-                        return res.status(500).json({ message, data: error.message })
-                    });
-            }]
+                const allUserOptionsCreated = await models['sessionUserOption'].bulkCreate(sessionUsersOptionsForCreate);
+                return allUserOptionsCreated;
 
+            } catch (error) {
+                const message = `An error has occured creating the options.`;
+                return res.status(500).json({ message, data: error.message })
+            }
+        }
+
+        try {
+            const userCreated = await createUser();
+            const institutHasUserCreated = await postInstitutHasUser(userCreated);
+            await checkSessionPlaceAvailable();
+            const allExamsFromSession = await findAllSessionExams();
+            const sessionUserCreated = await postSessionUser(userCreated);
+            await postAllUserOptions(sessionUserCreated, allExamsFromSession);
+
+            const message = `User has been created ,added to the institut, the session, and UserHasOptions have been created successfuly`;
+            res.json({ message, data: sessionUserCreated });
+
+        } catch (error) {
+            const message = `An error has occured .`;
+            return res.status(500).json({ message, data: error.message })
+        }
+    }
     );
-})}
+}
