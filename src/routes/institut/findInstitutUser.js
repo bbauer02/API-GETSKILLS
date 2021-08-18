16abab78b73bd2343a01df0513232f7e41151be1
debe0025 <deb.jules@outlook.fr>
2021-08-18 16:41:12 +0200

    findAllSessionByInstitut fixe users=true parameter

--- src/routes/institut/findInstitutUser.js
+++ src/routes/institut/findInstitutUser.js
@@ -1,18 +1,18 @@
-﻿const {models} = require('../../models');
+﻿const { models } = require('../../models');
 const { Op } = require('sequelize');
 const { isAuthenticated, isAuthorized } = require('../../auth/jwt.utils');
 
-module.exports =  (app) => {
+module.exports = (app) => {
 
-    app.get('/api/instituts/:institut_id/users/:user_id',isAuthenticated, isAuthorized, async (req,res) => {
+    app.get('/api/instituts/:institut_id/users/:user_id', isAuthenticated, isAuthorized, async (req, res) => {
         try {
-            const parameters = {}; 
+            const parameters = {};
             parameters.where = {
                 institut_id: req.params.institut_id,
                 user_id: req.params.user_id
             };
-            
-            parameters.attributes = {exclude:['password']};
+
+            parameters.attributes = { exclude: ['password'] };
             parameters.include = [
                 {
                     model: models['Institut']
@@ -21,42 +21,41 @@ module.exports =  (app) => {
                     model: models['Role']
                 },
                 {
-                    
                     model: models['User'],
-                    attributes:{exclude:['password']},
-                    include:[{
+                    attributes: { exclude: ['password'] },
+                    include: [{
                         model: models['Country'],
-                        as:'country',
-                        attributes : ["label"]
+                        as: 'country',
+                        attributes: ["label"]
                     },
                     {
                         model: models['Country'],
-                        as:'nationality',
-                        attributes : [["countryNationality",'label']]
+                        as: 'nationality',
+                        attributes: [["countryNationality", 'label']]
                     },
                     {
                         model: models['Country'],
-                        as:'firstlanguage',
-                        attributes : [["countryLanguage",'label']]
+                        as: 'firstlanguage',
+                        attributes: [["countryLanguage", 'label']]
                     },
                     {
                         model: models['Role'],
-                        as:'systemRole'
+                        as: 'systemRole'
                     }]
                 }
             ];
             const InstitutHasUser = await models['institutHasUser'].findOne(parameters);
-            if(InstitutHasUser === null) {
+            if (InstitutHasUser === null) {
                 const message = `institutHasUser doesn't exist.Retry with an other institut id or user id.`;
-                return res.status(404).json({message});
+                return res.status(404).json({ message });
             }
             const message = `institutHasUser found`;
-            res.json({message, data: InstitutHasUser});
+            res.json({ message, data: InstitutHasUser });
 
         }
-        catch(error) {
+        catch (error) {
             const message = `Service not available. Please retry later.`;
-            res.status(500).json({message, data: error.toString()})
+            res.status(500).json({ message, data: error.toString() })
         }
     });
 }
