e1fe39c120d1543fc51526fa841e534ecdee0350
debe0025 <deb.jules@outlook.fr>
2021-08-19 10:24:18 +0200

    added parameters for findUserByPK for extra info

--- src/routes/user/findUserByPk.js
+++ src/routes/user/findUserByPk.js
@@ -1,57 +1,85 @@
-﻿const {models} = require('../../models');
-const {  isAuthenticated,isAuthorized } = require('../../auth/jwt.utils');
-module.exports =  (app) => {
-    app.get('/api/users/:user_id', isAuthenticated,isAuthorized, async (req,res) => {
+﻿const { models } = require('../../models');
+const { isAuthenticated, isAuthorized } = require('../../auth/jwt.utils');
+module.exports = (app) => {
+    app.get('/api/users/:user_id', isAuthenticated, isAuthorized, async (req, res) => {
         try {
-            const parameters = {}; 
-            parameters.where = {user_id:req.params.user_id};
+            const parameters = {};
+            parameters.where = { user_id: req.params.user_id };
             parameters.order = ['lastname'];
             parameters.include = [
-            {
-                model: models['Country'],
-                as:'country',
-                attributes : ["label"]
-            },
-            {
-                model: models['Country'],
-                as:'nationality',
-                attributes : [["countryNationality",'label']]
-            },
-            {
-                model: models['Country'],
-                as:'firstlanguage',
-                attributes : [["countryLanguage",'label']]
-            },
-            {
-                model: models['institutHasUser'],
-                attributes:['institut_id'],
-                as:'instituts',
-                include:[{
-                    model:models['Institut'],
-                    attributes:['label']
+                {
+                    model: models['Country'],
+                    as: 'country',
+                    attributes: ["label"]
                 },
                 {
-                    model:models['Role'],
-                    attributes:['role_id','label','power']
-                }]
-            },
-            {
-                model:models['Role'], 
-                as:'systemRole',
-                attributes:['role_id','label','power']
-            }];
+                    model: models['Country'],
+                    as: 'nationality',
+                    attributes: [["countryNationality", 'label']]
+                },
+                {
+                    model: models['Country'],
+                    as: 'firstlanguage',
+                    attributes: [["countryLanguage", 'label']]
+                },
+                {
+                    model: models['institutHasUser'],
+                    attributes: ['institut_id'],
+                    as: 'instituts',
+                    include: [{
+                        model: models['Institut'],
+                        attributes: ['label']
+                    },
+                    {
+                        model: models['Role'],
+                        attributes: ['role_id', 'label', 'power']
+                    }]
+                },
+                {
+                    model: models['Role'],
+                    as: 'systemRole',
+                    attributes: ['role_id', 'label', 'power']
+                }];
+
+            // Add sessions
+            if (req.query.sessions === "true") {
+                const addSessions = {
+                    model: models['sessionUser'],
+                    where: { user_id: req.params.user_id },
+                    include: [
+                        {
+                            model: models['Session'],
+                            include: [
+                                {
+                                    model: models['Institut'],
+                                    attributes: ['institut_id', 'label']
+                                },
+                                {
+                                    model: models['Level'],
+                                    attributes: ['level_id', 'label']
+                                },
+                                {
+                                    model: models['Test'],
+                                    attributes: ['test_id', 'label']
+                                }
+                            ]
+                        }]
+                };
+                parameters.include.push(addSessions);
+            }
+
             // parameters.attributes = {exclude:['password']};
             const User = await models['User'].findOne(parameters);
-            if(User === null) {
+            if (User === null) {
                 const message = `User doesn't exist.Retry with an other user id.`;
-                return res.status(404).json({message});
+                return res.status(404).json({ message });
             }
             const message = `Users found`;
-            res.json({message, data: User});
+            res.json({ message, data: User });
         }
-        catch(error) {
+        catch (error) {
             const message = `Service not available. Please retry later.`;
-            res.status(500).json({message, data: error});
+            res.status(500).json({ message, data: error });
         }
     });
 }
