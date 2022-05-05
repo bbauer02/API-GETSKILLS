d3231706f21ebc329e92a31674a9d68bc3656abc
Bauer <bbauer02@gmail.com>
2022-05-05 11:45:32 +0200

    modification du chemin d'accès de la clé WebHook

--- src/services/stripe/webhook.js
+++ src/services/stripe/webhook.js
@@ -1,5 +1,6 @@
 ﻿const {models} = require('../../models');
 const stripeAPI  = require('../../../stripe');
+const config = require('../../../config.prod');
 
 async function  webhook(req, res) {
   const sig = req.headers['stripe-signature'];
@@ -8,7 +9,7 @@ async function  webhook(req, res) {
     event = stripeAPI.webhooks.constructEvent(
       req['rawBody'],
       sig,
-      process.env.WEB_HOOK_SECRET
+      config.db[process.env.NODE_ENV].whSecretKey
     )
   } catch(error) {
     return res.status(400).send(`WEBHOOK ERROR ${error.message}`);
